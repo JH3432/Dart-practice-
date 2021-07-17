4f2c3b294de2ebfcef3952f3af8bea9c12fffc81
JH3432 <87525005+JH3432@users.noreply.github.com>
2021-07-17 14:18:21 +0900

    21.07.17
    다트 첫 연습 쪼금 쉬운 것 같다 ;)

---
+++ 21_07_17.dart
@@ -0,0 +1,14 @@
+void main(){
+ String m ='상범이가 알려준 수식은';
+ 
+  
+ int b = 12;
+ double v = 3;
+ double c = 5.6;
+ 
+ print('$m $b / $v + $c 입니다.');
+ double z = b / v + c;
+ print('정답은 $z 입니다.');
+ print('정답은 ${b / v + c} 입니다.');
+  
+}
